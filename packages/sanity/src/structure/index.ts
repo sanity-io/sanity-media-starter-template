8018961c1fd8b5bf29cfa013a1205d06cb60b8ae
alex-ketch <alex.ketch@planetary.co>
2023-12-08 16:08:52 -0500

    feat(OG): support OpenGraph image previews

--- packages/sanity/src/structure/index.ts
+++ packages/sanity/src/structure/index.ts
@@ -1,44 +1,79 @@
-import { StructureBuilder } from 'sanity/desk';
-import EmailPreview from '../previews/emailPreview';
-import { IoNewspaperOutline } from 'react-icons/io5'
-import { EditIcon, EyeOpenIcon } from '@sanity/icons'
+import {StructureBuilder} from 'sanity/desk'
+import EmailPreview from '../previews/emailPreview'
+import {IoNewspaperOutline} from 'react-icons/io5'
+import {EditIcon, EyeOpenIcon} from '@sanity/icons'
+import OpenGraphPreview from '../previews/openGraphPreview'
 
-const excludedSchemaTypes = ['media.tag', 'newsletter'];
+const excludedSchemaTypes = ['media.tag', 'newsletter']
 
-const structure = (S: StructureBuilder) =>
-  S.list()
+/* A invalid document ID guaranteed to not exist in the dataset
+ * Used as fallback when filtering document types in list items, in case
+ * `listItem.getId()` returns `undefined` for some reason.
+ */
+const AbsurdDocument = Symbol('NEVER_MATCHED').toString()
+
+const structure = (S: StructureBuilder) => {
+  const allDocuments = S.documentTypeListItems().filter(
+    (listItem) => !excludedSchemaTypes.includes(listItem.getId() ?? AbsurdDocument),
+  )
+
+  return S.list()
     .title('Home')
     .items([
-      ...S.documentTypeListItems().filter(listItem => !excludedSchemaTypes.includes(listItem.getId())),
+      S.listItem()
+        .title('Articles')
+        .icon(IoNewspaperOutline)
+        .child(
+          S.documentTypeList('article')
+            .title('All Artlcles')
+            .defaultOrdering([{field: 'publishDate', direction: 'desc'}])
+            .menuItems([
+              // @ts-ignore
+              ...S.documentTypeList('article').getMenuItems(),
+              S.orderingMenuItem({
+                name: 'publishDate',
+                title: 'Publish Date',
+                by: [{field: 'publishDate', direction: 'desc'}],
+              }),
+            ])
+            .child((documentId) =>
+              S.document()
+                .documentId(documentId)
+                .schemaType('article')
+                .views([
+                  S.view.form().icon(EditIcon),
+                  S.view.component(OpenGraphPreview).title('Open Graph Preview').icon(EyeOpenIcon),
+                ]),
+            ),
+        ),
+      ...allDocuments,
       S.listItem()
         .title('Newsletters')
         .icon(IoNewspaperOutline)
         .child(
           S.documentTypeList('newsletter')
             .title('All Newsletters')
-            .defaultOrdering([{ field: 'publishDate', direction: 'desc' }])
+            .defaultOrdering([{field: 'publishDate', direction: 'desc'}])
             .menuItems([
               // @ts-ignore
               ...S.documentTypeList('newsletter').getMenuItems(),
               S.orderingMenuItem({
                 name: 'publishDate',
                 title: 'Publish Date',
-                by: [{ field: 'publishDate', direction: 'desc' }],
+                by: [{field: 'publishDate', direction: 'desc'}],
               }),
             ])
-            .child(documentId =>
+            .child((documentId) =>
               S.document()
                 .documentId(documentId)
                 .schemaType('newsletter')
                 .views([
-                  S.view.form()
-                    .icon(EditIcon),
-                  S.view.component(EmailPreview)
-                    .title('Preview')
-                    .icon(EyeOpenIcon)
-                ])
-            )
+                  S.view.form().icon(EditIcon),
+                  S.view.component(EmailPreview).title('Preview').icon(EyeOpenIcon),
+                ]),
+            ),
         ),
-    ]);
+    ])
+}
 
-export default structure;
+export default structure
